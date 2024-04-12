abaa81a2fd7574aceb83e712587b1dfb29df660f
Ernesto20007 <arpad.erneszt@gmail.com>
2024-04-12 09:27:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from pyscript import document
+
+
+def szamit (event):
+    szam1=float(document.queryselector("#szam1").value)
+    szam1=float(document.queryselector("#szam2").value)
+    muvelet=float(document.queryselector("#muvelet").value)
+    document.queryselector("#muvelet").innerText="Tibi"
     
+      
+def torol(event):
+    document.queryselector("#szam1").value=""
+    document.queryselector("#szam2").value=""
+    document.queryselector("#eredmeny").innerText=""
+    document.queryselector("#muvelet").selectedIndex=0
+    
+
